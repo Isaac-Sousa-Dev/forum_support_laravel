b5ffb9c3a611c03f75bc24f509660d76b2d30b1e
Isaac-Sousa-Dev <isaac.sousa.1202@gmail.com>
2024-02-15 00:25:30 -0300

    second commit

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Admin\SupportController;
+use App\Http\Controllers\SiteController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/contato', [SiteController::class, 'index']);
+
+Route::get('/supports', [SupportController::class, 'index'])->name('supports.index');
